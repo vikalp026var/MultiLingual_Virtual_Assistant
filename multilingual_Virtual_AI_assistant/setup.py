b7bd606aa60282bff5ec1cafbde58bfc7fb7e10a
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2024-04-14 11:52:56 +0530

    Add files via upload

---
+++ multilingual_Virtual_AI_assistant/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+     name="GenAIProject",
+     version='0.0.1',
+     author='Vikalp',
+     author_email='vikalp026varshney@gmail.com',
+     packages=find_packages(),
+     include_dirs=[]
+)
